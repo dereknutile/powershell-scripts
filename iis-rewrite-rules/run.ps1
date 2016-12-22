b02b97e5fb073b4c24618ce566d4650417691094
Derek Nutile <dereknutile@gmail.com>
2016-12-21 16:54:33 -0800

    e/ps1 Complete first pass at IIS rewriter

--- iis-rewrite-rules/run.ps1
+++ iis-rewrite-rules/run.ps1
@@ -11,27 +11,35 @@
 .LINK
     https://github.com/dereknutile/powershell-scripts
 .EXAMPLE
-    powershell.exe .\run.ps1 serverName csvFile webSiteName
+    powershell.exe .\run.ps1 serverName csvFile webSiteName logFile
+.EXAMPLE
+    Add the -Force switch to overwrite the rule if it exists
+    powershell.exe .\run.ps1 serverName -Force
 .EXAMPLE
     Provide output to the console.
-    powershell.exe .\run.ps1 serverName csvFile webSiteName -Verbose
+    powershell.exe .\run.ps1 serverName csvFile webSiteName logFile -Verbose
 ----------------------------------------------------------------------------- #>
 
 
 <# -----------------------------------------------------------------------------
-  Handle commandline parameters and verbosity.
+    Handle commandline parameters and verbosity.
 ----------------------------------------------------------------------------- #>
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$True)]
-    [string]$serverName,
+    [Parameter(Mandatory=$False)]
+    [string]$serverName = $env:computerName,
 
     [Parameter(Mandatory=$False)]
-    # [string]$csvFile = (Get-Item -Path ".\rules.csv" -Verbose).FullName,
     [string]$csvFile = (Get-Item -Path ".\rules.csv" -Verbose).FullName,
+    # [string]$csvFile = (Get-Item -Path ".\rules.example" -Verbose).FullName,
 
     [Parameter(Mandatory=$False)]
-    [string]$webSiteName = "Default Web Site"
+    [string]$webSiteName = "Default Web Site",
+
+    [Parameter(Mandatory=$False)]
+    [string]$logFile = ".\logfile.log",
+    
+    [switch]$Force = $False
 )
 
 
@@ -50,21 +58,62 @@ if(Get-PowershellVersion -eq 2) {
     Set variables.
 ----------------------------------------------------------------------------- #>
 $now = Get-Date
-$counter = 0
-
+$rulesCounter = 0
+$site = "iis:\sites\$webSiteName"
 
 <# -----------------------------------------------------------------------------
     Script functions.
 ----------------------------------------------------------------------------- #>
-Function Parse-CsvFile ([string]$file) {
-    Import-CSV $csvFile | Foreach-Object { 
-        $script:counter++
-        Write-Host "Name: $($_.name)"
+Function Parse-CsvFile {
+    Import-CSV $script:csvFile | Foreach-Object { 
+        $script:rulesCounter++
+
+        if($Force){
+            Delete-IisRewrite $_.Name
+        }
+
+        Add-IisRewrite $_.PatternSyntax $_.StopProcessing $_.Name $_.Pattern $_.Type $_.IgnoreCase $_.RewriteUrl
     }
+}
+
+Function Add-IisRewrite (
+    [string]$patternSyntax = "Regular Expressions",
+    [string]$stopProcessing = "True",
+    [string]$name,
+    [string]$pattern,
+    [string]$type = "Rewrite",
+    [string]$ignoreCase = "True",
+    [string]$RewriteUrl )
+{
+        
+    Add-WebConfigurationProperty -pspath $site -filter "system.webServer/rewrite/rules" -name "." -value @{
+        name="$name";
+        patternSyntax='Regular Expressions';
+        stopProcessing='True';
+    }
+
+    Set-WebConfigurationProperty -pspath $script:site -filter "/system.webserver/rewrite/rules/rule[@name='$name']/match" -name "." -value @{ url="$pattern";ignoreCase="$ignoreCase"; }
+
+    Set-WebConfigurationProperty -pspath $script:site -filter "/system.webserver/rewrite/rules/rule[@name='$name']/action" -name "." -value @{ type="$type"; url="$RewriteUrl";} 
+}
+
+Function Delete-IisRewrite ([string]$ruleName) {
+    Clear-WebConfiguration -pspath $script:site -filter "/system.webserver/rewrite/rules/rule[@name='$ruleName']"
+    Write-Log "Deleted rule: $ruleName"
+}
+
+Function Process-CsvFile ([string]$file) {
+    Parse-CsvFile
 }
 
 <# -----------------------------------------------------------------------------
     Run.
 ----------------------------------------------------------------------------- #>
-Parse-CsvFile
-Write-Host "$counter lines processed."
+Write-Log "-----------------------------------------------------------"
+Write-Log "Start import of $csvFile."
+Write-Log "Processing for server: $serverName"
+Process-CsvFile
+Write-Log "$rulesCounter rules processed."
+Write-Log "End of Import"
+Write-Log "-----------------------------------------------------------"
+Write-Log ""
